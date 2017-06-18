6a96b3ed954a5733ab84dc84f1413705b1ef39fe
Raja M <Raja1612.M@gmail.com>
2017-06-18 11:10:16 +0530

    media-caf_msm8952: Restrict to specific device

--- Android.mk
+++ Android.mk
@@ -1,3 +1,5 @@
+ifneq ($(filter mpq8092 msm8960 msm8226 msm8x26 msm8610 msm8974 msm8x74 apq8084 msm8916 msm8994 msm8992 msm8909 msm8952,$(TARGET_BOARD_PLATFORM)),)
+ifeq ($(TARGET_BOARD_PLATFORM),msm8952)
 QCOM_MEDIA_ROOT := $(call my-dir)
 $(warning target list is : $(MSM_VIDC_TARGET_LIST))
 
@@ -11,4 +13,6 @@ ifeq ($(call is-board-platform-in-list, $(MSM_VIDC_TARGET_LIST)),true)
 include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
 include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
 
+endif
+endif
 endif
